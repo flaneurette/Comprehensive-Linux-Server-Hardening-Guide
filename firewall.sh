3ef90906f735b162b09257ba156c0a95c6c1a959
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2026-01-14 05:01:18 +0100

    Create firewall.sh

---
+++ firewall.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Block everything, except for user-defined IP addresses and public ports.
+
+# Your private allowed IPs (home, work, etc)
+ALLOWED_IPS=("105.1.2.3" "105.1.2.4")
+
+# Allowed Ports for above IPs
+PORTS=(22 993 587 995)
+
+# Allowed Public ports
+PUBLIC_PORTS=(80 443 25)
+
+# Install netfilter-persistent if not installed
+
+apt remove -y ufw
+apt install -y netfilter-persistent
+
+sleep 3
+
+# Flush existing rules
+iptables -F
+iptables -X
+
+# Default policies
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Allow loopback
+iptables -A INPUT -i lo -j ACCEPT
+
+# Allow established connections
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# --- Allow your IPs on specified ports ---
+for ip in "${ALLOWED_IPS[@]}"; do
+  for port in "${PORTS[@]}"; do
+    iptables -I INPUT 1 -p tcp -s "$ip" --dport "$port" -j ACCEPT
+  done
+done
+
+# --- Optional: allow ping ---
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+# --- Allow public services ---
+for port in "${PUBLIC_PORTS[@]}"; do
+  iptables -A INPUT -p tcp --dport "$port" -j ACCEPT
+done
+
+# Save rules
+netfilter-persistent save
